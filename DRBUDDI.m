ec24b839e2b34c7cca7bd0dbaacf704164b56831
Xuan Gu <guxuan@msn.com>
2019-10-21 13:36:14 +0200

    Update DRBUDDI.m

--- DRBUDDI.m
+++ DRBUDDI.m
@@ -7,58 +7,58 @@ pe2_corrected_dir = [results_dir, filename,'_',pe2, '_DRBUDDI_corrected.nii.gz']
 pe1_new_dir = [results_dir, filename,'_',pe1, '.nii.gz'];
 pe2_new_dir = [results_dir, filename,'_',pe2, '.nii.gz'];
 
+slash_index = strfind(bvals_dir, '/');
+bvals_filename = bvals_dir(slash_index(end)+1:end);
+bvals_new_dir = [results_dir, bvals_filename];
+slash_index = strfind(bvecs_dir, '/');
+bvecs_filename = bvecs_dir(slash_index(end)+1:end);
+bvecs_new_dir = [results_dir, bvecs_filename];
 
 % DIFFPREP changes radiological to neurological so we cp data to DRBUDDIfolder first
 system(['cp ', pe1_dir, ' ', pe1_new_dir]);
 system(['cp ', pe2_dir, ' ', pe2_new_dir]);
+system(['cp ', bvals_dir, ' ', bvals_new_dir]);
+system(['cp ', bvecs_dir, ' ', bvecs_new_dir]);
 
-if strcmp(bvals_dir, '')
+% Add a dummy volume
+system(['fslroi ', pe1_new_dir, ' ', results_dir, 'temp.nii.gz 0 1']);
+system(['fslmerge -t ', pe1_new_dir, ' ', pe1_new_dir, ' ', results_dir, 'temp.nii.gz']);
+system(['rm ', results_dir, 'temp.nii.gz']);
+bvals = load(bvals_new_dir);
+bvals = [bvals, bvals(1)];
+save(bvals_new_dir, 'bvals', '-ascii');
+bvecs = load(bvecs_new_dir);
+bvecs = [bvecs, bvecs(:,1)];
+save(bvecs_new_dir, 'bvecs', '-ascii');
+
+pe1_list_dir = [results_dir, filename,'_',pe1, '_proc.list'];
+pe2_list_dir = [results_dir, filename,'_',pe2, '_proc.list'];
+
+if (contains(pe1, 'LR') || contains(pe2, 'LR'))
     
-    % create bvals and bvecs
-    bvals_dir = [results_dir, 'b0.bval'];
-    bvecs_dir = [results_dir, 'b0.bvec'];
-    bvals_b0 = zeros(1,2);
-    bvecs_b0 = zeros(3,2);
-    bvecs_b0(1,:) = 1;
-    save(bvals_dir, 'bvals_b0', '-ascii');
-    save(bvecs_dir, 'bvecs_b0', '-ascii');
+    system(['DIFFPREP --dwi ', pe1_new_dir, ' --bvecs ', bvecs_new_dir, ' --bvals ', bvals_new_dir, ' -p horizontal -s ', T2_dir, ' --epi off ', ' --step 0 --will_be_drbuddied 1']);
+    system(['DIFFPREP --dwi ', pe2_new_dir, ' --bvecs ', bvecs_dir, ' --bvals ', bvals_dir, ' -p horizontal -s ', T2_dir, ' --epi off ', ' --step 0 --will_be_drbuddied 1']);    
     
+elseif (contains(pe1, 'AP') || contains(pe2, 'AP'))
+    system(['DIFFPREP --dwi ', pe1_new_dir, ' --bvecs ', bvecs_new_dir, ' --bvals ', bvals_new_dir, ' -p vertical -s ', T2_dir, ' --epi off ', ' --step 0 --will_be_drbuddied 1']);
+    system(['DIFFPREP --dwi ', pe2_new_dir, ' --bvecs ', bvecs_dir, ' --bvals ', bvals_dir, ' -p vertical -s ', T2_dir, ' --epi off ', ' --step 0 --will_be_drbuddied 1']);
+        
 end
 
-[~, nvols] = system(['fslnvols ', pe1_new_dir]);
-nvols = str2num(nvols);
-
-if (nvols == 1)
-    system(['fslmerge -t ', pe1_new_dir, ' ', pe1_new_dir, ' ', pe1_new_dir]);
-    system(['fslmerge -t ', pe2_new_dir, ' ', pe2_new_dir, ' ', pe2_new_dir]);
-end
-
-
-system(['DIFFPREP --dwi ', pe1_new_dir, ' --bvecs ', bvecs_dir, ' --bvals ', bvals_dir, ' -p vertical -s ', T2_dir, ' --epi off ', ' --step 0 --will_be_drbuddied 1']);
-system(['DIFFPREP --dwi ', pe2_new_dir, ' --bvecs ', bvecs_dir, ' --bvals ', bvals_dir, ' -p vertical -s ', T2_dir, ' --epi off ', ' --step 0 --will_be_drbuddied 1']);
-
-pe1_list_dir = [results_dir, pe1_filename, '_proc.list'];
-pe2_list_dir = [results_dir, pe2_filename, '_proc.list'];
-system(['DR_BUDDI --up_data ', pe1_list_dir, ' --down_data ', pe2_list_dir, ' --structural ', T2_dir]);
-
-
+system(['DR_BUDDI_withoutGUI --up_data ', pe1_list_dir, ' --down_data ', pe2_list_dir, ' --structural ', T2_dir, ' --output ', results_dir, pe1, pe2, '/dwi_', pe1, pe2, '_DRBUDDI_corrected.list']);
 
 
 %    Neurological to Radiological
-system(['fslswapdim ', results_dir, pe1_filename, '_proc_DRBUDDI.nii x -y z ', pe1_corrected_dir]);
+system(['fslswapdim ', results_dir, pe1, pe2,'/', filename,'_',pe1, '_proc_DRBUDDI_up_final.nii x -y z ', pe1_corrected_dir]);
 system(['fslorient -swaporient ', pe1_corrected_dir]);
 
-system(['fslswapdim ', results_dir, pe2_filename, '_proc_DRBUDDI.nii x -y z ', pe2_corrected_dir]);
+system(['fslswapdim ', results_dir, pe1, pe2,'/', filename,'_',pe2, '_proc_DRBUDDI_down_final.nii x -y z ', pe2_corrected_dir]);
 system(['fslorient -swaporient ', pe2_corrected_dir]);
 
 
-if (nvols == 1)
-    system(['fslroi ', pe1_corrected_dir, ' ',  pe1_corrected_dir, ' 0 1']);
-    system(['fslroi ', pe2_corrected_dir, ' ',  pe2_corrected_dir, ' 0 1']);
-end
-
-
-
+% Remove dummy volume
+[~, nvols] = system(['fslnvols ', pe1_corrected_dir]);
+system(['fslroi ', pe1_corrected_dir, ' ', pe1_corrected_dir, ' 0 ', num2str(str2num(nvols)-1)]);
 
 
 
